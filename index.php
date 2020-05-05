bd1de94c5dfbcd822c6bacf93dfec565a8453e23
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-05 16:32:02 +0200

    ex1 partie1 php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$objet = 'text'; //déclaration variable
+$message = 'text2'; //variable objet et message
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex1 partie1 php</title>
+</head>
+<body>
+  <?php
+  echo 'bonjour, '; //initialiser la valeur texte
+  echo 'en route pour une aventure';
+  ?>
+</body>
+</html>
